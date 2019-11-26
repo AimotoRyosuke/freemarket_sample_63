be9d16fd606954cbc0eb87b821fcba15a803faac
kiyohashi <iz.kk.r77@gmail.com>
2019-11-26 22:37:19 +0900

    item-delete-function
    アイテム削除機能を実装しました。
    実装にあたって出品者自身で商品詳細ページに訪れたときのビューを作成しました。
    編集ボタン、削除ボタン、一時停止ボタンを作りました。
    user機能が出来次第条件分けしたいので
    条件式は仮置きで設定しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,10 @@ class ItemsController < ApplicationController
   end
   def show
     @item = Item.find(params[:id])
-  end  
+  end
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 end
