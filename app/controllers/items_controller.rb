a38e16ca8b2feeb2925dd20213bdb5991764c826
kiyohashi <iz.kk.r77@gmail.com>
2019-11-27 13:47:14 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :destroy]
+
   def index
   end
+
   def show
-    @item = Item.find(params[:id])
   end
+
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show
+    end
+  end
+  
+  private
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
