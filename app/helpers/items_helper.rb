8de3fa51f881b44e8f4f08452906938ebd23655e
kiyohashi <iz.kk.r77@gmail.com>
2019-11-30 17:51:57 +0900

    edit-item-show
    アクティブハッシュを導入したのでshipping_costについて詳細ページでの表示を分けるメソッドを定義し実装しました。

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,9 @@
 module ItemsHelper
+  def price_shipping_cost(item)
+    if item.shipping_cost.name == '送料込み(出品者負担)'
+      item.shipping_cost.name.slice(0..3)
+    else
+      item.shipping_cost.name.slice(0..2)
+    end
+  end
 end
