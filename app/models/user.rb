e176afc70b034c0d784c6749a2f5b4d2383273ae
kiyohashi <iz.kk.r77@gmail.com>
2019-12-01 16:18:19 +0900

    create-purchase-function

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_one  :address
   has_one  :credit
   has_many :items
+  has_many :purchases
 
   before_validation :change_string
   validates :nickname, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth, presence: {message: "%{attribute}を入力してください"}
