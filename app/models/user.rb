f542a6f42b7fbe64c988b1b7cfac6b4b9590ea77
AimotoRyosuke <r.aimoto97vg@gmail.com>
2019-12-12 20:30:25 +0900

    user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
-        #  :omniauthable, omniauth_providers: [:google_oauth2, :facebook]
-  has_one  :address
+  has_one :address
   has_one :card
   has_one  :credit
   has_many :items
@@ -12,11 +11,11 @@ class User < ApplicationRecord
 
   before_validation :change_string
   validates :nickname, :password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birth, presence: {message: "%{attribute}を入力してください"}
-  validates :tel, presence: {message: "会員登録できません"}
+  validates :tel, presence: {message: "会員登録できません"}, uniqueness: {message: "この電話番号はすでに存在します"}
   validates :nickname, ban_reserved: true
   validates :nickname, mercari_fomat: true
   validates :email, format: { with: /\A[\w+\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]+\z/i, message: "フォーマットが不適切です"}
-  validates :email, {uniqueness: {message: "メールアドレスに誤りがあります。ご確認いただき、正しく変更してください。"}}
+  validates :email, {uniqueness: {message: "このメールアドレスはすでに存在します。ご確認いただき、正しく変更してください。"}}
   validates :password, length: {in: 7..128, message: "パスワードは7文字以上128文字以下で入力してください"}
   validates :password, {format: { with: /\A[a-zA-Z0-9]+\z/, message: "英字と数字両方を含むパスワードを設定してください"}}
   validates :first_name, :last_name, :first_name_kana,:last_name_kana, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/, message: "%{attribute}に数字や特殊文字は使用できません"}
